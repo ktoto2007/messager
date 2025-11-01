bf97ba574b1f8d2510039d3875746d36b6e09228
ktoto2007 <shagako2007@gmail.com>
2025-11-01 10:41:32 +0300

    tiny changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import './App.css'
-import { useMessage } from './stores'
+import { useMessage, type MessageType } from './stores'
 import { useShallow } from 'zustand/react/shallow'
 
 const dialogs = [{id: '3a49968e-be78-4593-8440-8f60d168a8f2', name: 'Чувак'}, {id: '3a49968e-be78-4593-8440-8f60d168a8f2', name: 'Чувак'}]
@@ -36,11 +36,6 @@ const Message = (props: MessageProps) => {
   );
 }
 
-type SearchProps = {
-  mutateValue: React.Dispatch<React.SetStateAction<string>>
-}
-
-
 const Messages = () => {
   const {messages, addMessage} = useMessage(useShallow(state => ({
     messages: state.messages,
@@ -66,19 +61,14 @@ const MessageInput = (props: MessageInputProps) => {
   );
 }
 
-const Search = (props: SearchProps) => {
-  return (
-    <div className="search-contanier">
-      <input className="search" placeholder='Поиск' onChange={(e)=>props.mutateValue(e.target.value)}/>
-    </div>
-  );
-}
-
 const ChatBottom = () => {
   return (
     <div className="chat-bottom">
-      <MessageInput/>
-      <button className="send button"><img src="src\assets\Email Send.svg" alt=""/></button>
+      <input className="user-input-field" placeholder="Сообщение" onChange={(e)=>setMessage(e.target.value)}/>
+      <button onClick={e=> {
+        addMessage({id: 'ei21094', text: message, time: '10:00'})
+        setMessage("")
+      }} className="send button"><img src="src\assets\Email Send.svg" alt=""/></button>
       <div className="button">
         <img className="" src="src\assets\Microphone.svg" alt=""/>
       </div>
@@ -99,26 +89,38 @@ const Nav = () => {
   );
 }
 
+type SearchProps = {
+  mutateValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Search = (props: SearchProps) => {
+  return (
+    <div className="search-contanier">
+      <input className="search" placeholder='Поиск' onChange={(e)=>props.mutateValue(e.target.value)}/>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="contanier">
         <Nav/>
         <div className="chat-contanier">
-            <div className="chat-header">
-                <div className="header-left">
-                    <img className="chat-profile" src="src\assets\Profile.svg" alt=""/>
-                    <div className="name-and-activity">
-                        <p className="name-header">Чувак</p>
-                        <p className="activity">В сети</p>
-                    </div>
-                </div>
-                <img className="parameters" src="src\assets\Ellipsis.svg" alt=""/>
+          <div className="chat-header">
+            <div className="header-left">
+              <img className="chat-profile" src="src\assets\Profile.svg" alt=""/>
+              <div className="name-and-activity">
+                <p className="name-header">Чувак</p>
+                <p className="activity">В сети</p>
+              </div>
             </div>
-            <div className="content">
-                <div className="filler"></div>
-                <Messages/>
-            </div>
-            <ChatBottom/>
+            <img className="parameters" src="src\assets\Ellipsis.svg" alt=""/>
+          </div>
+          <div className="content">
+            <div className="filler"></div>
+            <Messages/>
+          </div>
+          <ChatBottom/>
         </div>
     </div>
   )
